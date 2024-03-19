d6cec1b555512564348b703571b19bf7be734170
Nyabondeng <n.adut1@alustudent.com>
2024-03-18 17:42:04 -0700

    All filed

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table in current database
+-- Creates a table first_table in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
