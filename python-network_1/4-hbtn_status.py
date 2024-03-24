6bf5efe0ab0b7f36de5f0fe7e47a24c0316b2ffd
Nyabondeng <n.adut1@alustudent.com>
2024-03-24 14:30:44 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""I documented you"""
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
 import requests
 
-if __name__ == '__main__':
-    res = requests.get("https://intranet.hbtn.io/status")
+if _name_ == '_main_':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+
+    res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
