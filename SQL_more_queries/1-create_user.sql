d420d5a121c84387ba3acddbad3f2dc9f3635c8e
Nyabondeng <n.adut1@alustudent.com>
2024-03-24 20:37:15 +0200

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,3 @@
 -- creates the MySQL server user user_0d_1 and grant all priviledges
-MYSQL_USER="user_0d_1"
-MYSQL_PASSWORD="user_0d_1_pwd"
-
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
