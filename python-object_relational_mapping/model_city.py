e15981c7df047933cd03e77c2cc9f6a6900c1b81
Nyabondeng <n.adut1@alustudent.com>
2024-03-26 19:04:07 +0200

    Update model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-contains the class City
-"""
+"""Contains the class definition of a city"""
 
-import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from model_state import Base
 
 
 class City(Base):
-    """Representation of a city"""
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    """Class City"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
